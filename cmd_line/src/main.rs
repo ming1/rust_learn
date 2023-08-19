23b7c9014fe23a9619a9804d3c096fe8b4b9d435
Ming Lei <tom.leiming@gmail.com>
2023-08-19 10:50:42 +0000

    cmd_line: clap derive based nested command line
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

---
+++ cmd_line/src/main.rs
@@ -0,0 +1,95 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum SubCommands {
+    /// does testing things
+    Loop {
+        /// lists test values
+        #[arg(short, long)]
+        back: bool,
+    },
+
+    Null {
+        /// lists test values
+        #[arg(short, long)]
+        comp: bool,
+    },
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+
+    #[clap(subcommand)]
+    Add(SubCommands),
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(name) = cli.name.as_deref() {
+        println!("Value for name: {name}");
+    }
+
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        Some(Commands::Add(add)) => match add {
+            SubCommands::Loop { back } => {
+                println!("add loop {}", back)
+            }
+            SubCommands::Null { comp } => {
+                println!("add null {}", comp)
+            }
+        },
+        None => {}
+    }
+
+    // Continued program logic goes here...
+}
